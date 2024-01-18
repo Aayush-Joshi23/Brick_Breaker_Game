af7c85285b3897f4cee52c067ff4b88abce4a21a
Aayush Joshi <69706553+Aayush-Joshi23@users.noreply.github.com>
2024-01-18 22:05:28 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
 
@@ -14,8 +13,7 @@ public class Main {
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         obj.add(gameplay);
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        
 
     }
 
